6200f92c2b35c133eb1d48b2cf2ee01722a164c3
apphosting-vh <lenovotabpro99@gmail.com>
2026-03-09 11:47:03 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
    Pattern: mm-v{app-version}-{deploy-counter}
    e.g.  mm-v2.0.1-1  →  mm-v2.0.2-1  →  mm-v2.0.2-2
    ─────────────────────────────────────────────────────────────────────── */
-const CACHE_NAME = 'mm-v3.15.6';
+const CACHE_NAME = 'mm-v3.15.8';
 
 /* ── 2. PRECACHE URLS ──────────────────────────────────────────────────────
    These are fetched and stored during the install phase so the app can

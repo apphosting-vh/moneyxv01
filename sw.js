669f0833085c1b483c7c7d0138418a8857ed3f2f
apphosting-vh <lenovotabpro99@gmail.com>
2026-03-23 17:50:46 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    • Same-origin assets     → cache-first, fallback to network
    ══════════════════════════════════════════════════════════════════════════ */
 
-const CACHE_NAME = 'mm-v3-34-8';
+const CACHE_NAME = 'mm-v3-34-9';
 
 /* Assets to precache on install */
 const PRECACHE_URLS = [
